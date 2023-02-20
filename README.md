6b56ace8e97b5879611d3e5a484d6aa633fc84f8
Raphael <raphael@quiqup.com>
2023-02-20 20:07:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Brightline test
+
+## How to run
+
+- Clone repository locally
+- Copy the full path for the `index.html`file
+- Paste it in your preferable browser
+
+## User guide
+
+- Use `Left` and `Right` arrow keys to navigate between cards
+- Use `Enter` to open the selected card
+- Use `Backspace` to go to the main cards list
+
+![alt text](https://github.com/[username]/[reponame]/blob/[branch]/image.jpg?raw=true)
---
+++ src/index.js
@@ -0,0 +1,117 @@
+var iconSvg = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="24px" width="24px">
+<path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+<path stroke-linecap="round" stroke-linejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
+</svg>
+`;
+
+let jsonData;
+
+var itemPage = document.getElementById("item");
+var mainCarousel = document.getElementById("carousel");
+
+const openVideoTab = (item) => {
+  document.getElementById("items").style.display = "none";
+
+  let itemTitle = document.createElement("h1");
+  itemTitle.textContent = item.name;
+  itemPage.style.display = "block";
+  itemPage.appendChild(itemTitle);
+
+  let video = document.createElement("video");
+  video.src = item.mediaFile;
+  video.controls = true;
+  video.muted = false;
+  video.addEventListener(
+    "error",
+    function () {
+      alert("Video type not supported");
+    },
+    true
+  );
+
+  itemPage.appendChild(video);
+};
+
+const closeVideoTab = () => {
+  document.getElementById("item").style.display = "none";
+  document.getElementById("items").style.display = "block";
+  document.getElementById("item").innerHTML = "";
+};
+
+async function streamsData() {
+  loadingData = true;
+  const response = await fetch(
+    "https://cdn-media.brightline.tv/training/demo.json",
+    {
+      method: "GET",
+    }
+  );
+  jsonData = await response.json();
+  loadingData = false;
+}
+
+const displayData = () => {
+  jsonData.streams.forEach((item) => {
+    if (item.name && item.mediaFile) {
+      let itemHtml = document.createElement("div");
+      itemHtml.className = "item-card";
+      itemHtml.innerHTML = `<div>${iconSvg}</div><div class="item-title">${item.name}</div>`;
+      itemHtml.addEventListener(
+        "click",
+        function () {
+          openVideoTab(item);
+        },
+        false
+      );
+
+      mainCarousel.appendChild(itemHtml);
+    }
+  });
+};
+
+var currentItem = 0;
+
+const navigateItems = () => {
+  var selectedItem = document.getElementsByClassName("item-card")[currentItem];
+  selectedItem.focus();
+  selectedItem.classList.add("item-card-focused");
+  if (currentItem > 0)
+    selectedItem.previousElementSibling.classList.remove("item-card-focused");
+  if (currentItem < jsonData.streams.length - 1) {
+    selectedItem.nextElementSibling.classList.remove("item-card-focused");
+  }
+};
+
+document.addEventListener("keydown", function (event) {
+  if (event.keyCode == 37) {
+    if (currentItem == 0) return false;
+    currentItem--;
+    navigateItems();
+    return false;
+  }
+
+  if (event.keyCode == 39) {
+    if (currentItem == jsonData.streams.length - 1) return false;
+    currentItem++;
+    navigateItems();
+    return false;
+  }
+
+  if (event.keyCode == 13) {
+    openVideoTab(jsonData.streams[currentItem]);
+    return false;
+  }
+
+  if (event.keyCode == 8) {
+    closeVideoTab();
+    return false;
+  }
+});
+
+async function app() {
+  await streamsData();
+  displayData();
+  navigateItems();
+}
+
+app();
